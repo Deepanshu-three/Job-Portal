05dba12faba25234c5b46f9cd45dc2a61e96e982
Deepanshu-three <deepanshu.main@gmail.com>
2024-09-03 01:53:51 +0530

    added application controller and route

--- backend/index.js
+++ backend/index.js
@@ -6,6 +6,8 @@ import connectDB from "./utils/db.js";
 import userRoute from "./routes/user.route.js"
 import companyRoute from "./routes/company.route.js"
 import jobRoute from "./routes/job.route.js"
+import applicaiton from "./routes/application.route.js"
+
 dotenv.config({});
 
 const app = express(); 
@@ -35,6 +37,7 @@ const PORT = process.env.PORT || 3000;
 app.use("/api/v1/user", userRoute)
 app.use("/api/v1/company", companyRoute)
 app.use("/api/v1/job", jobRoute)
+app.use("/api/v1/applicaiton", applicaiton)
 
 app.listen(PORT, () => {
     connectDB()
---
+++ backend/routes/application.route.js
@@ -0,0 +1,12 @@
+import e from "express";
+import isAuthenticated from "../middleware/usAuthenticated";
+import { applyJob, getApplicants, getAppliedJobs, updateStatus } from "../controllers/application.controller.js";
+
+const router = e.Router();
+
+router.route("/apply/:id").get(isAuthenticated, applyJob)
+router.route("/get").get(isAuthenticated, getAppliedJobs)
+router.route("/:id/applicants").get(isAuthenticated, getApplicants)
+router.route("/status/:id/update").post(isAuthenticated, updateStatus)
+
+export default router
