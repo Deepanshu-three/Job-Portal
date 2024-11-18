f0c69be9beeb13c21915c12910258637cbb6c527
Deepanshu-three <deepanshu.main@gmail.com>
2024-11-18 10:20:25 +0530

    fix navbar home button

--- frontend/src/components/CategoryCarousel.jsx
+++ frontend/src/components/CategoryCarousel.jsx
@@ -27,7 +27,7 @@ const CategoryCarousel = () => {
                 <CarouselContent>
                     {
                         category.map((cat, index) => (
-                            <CarouselItem className="md:basis-1/2 lg-basis-1/3">
+                            <CarouselItem key={index} className="md:basis-1/2 lg-basis-1/3">
                                 <Button onClick={()=>searchJobHandler(cat)} variant="outline" className="rounded-full">{cat}</Button>
                             </CarouselItem>
                         ))
